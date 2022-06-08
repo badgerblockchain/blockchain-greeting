140468de4ffcb0732fa5d432bfbb3a7d04301078
jack.gundrum <jpgundrum@wisc.edu>
2022-06-07 21:30:05 -0500

    commit to badger repo

---
+++ contracts/Greeter.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:\n", _greeting);
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) { // public means it can be read from react app and view means it is read only
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+}
